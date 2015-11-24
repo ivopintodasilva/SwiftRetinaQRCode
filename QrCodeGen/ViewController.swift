32054edaee4c68b26b147964707c25c5d0fe9f2a
ivopintodasilva <ivopintodasilva@gmail.com>
2015-11-24 11:04:24 +0000

    unnecessary code removed

--- QrCodeGen/ViewController.swift
+++ QrCodeGen/ViewController.swift
@@ -30,51 +30,39 @@ class ViewController: UIViewController {
     
     @IBAction func generateQR() {
         
-        /// CIQRCodeGenerator generates 27x27px images per default
-        let DefaultQRCodeSize = CGSize(width: 27, height: 27)
-        
         /// Data contained in the generated QRCode
         var data: NSData
-        
-        data = "cunaxa".dataUsingEncoding(NSUTF8StringEncoding)!
-        
+        if(i==6){
+            i=0
+        }
+        data = names[i++].dataUsingEncoding(NSUTF8StringEncoding)!
         /// Foreground color of the output
         /// Defaults to black
         let color = CIColor(red: 0, green: 0, blue: 0)
-        
         /// Background color of the output
         /// Defaults to white
         let backgroundColor = CIColor(red: 0.5, green: 1, blue: 1)
-        
-        
-        
-        
         let qrFilter = CIFilter(name: "CIQRCodeGenerator")
-        
         qrFilter!.setDefaults()
         qrFilter!.setValue(data, forKey: "inputMessage")
         qrFilter!.setValue("H", forKey: "inputCorrectionLevel")
-        
         let colorFilter = CIFilter(name: "CIFalseColor")
-        
         colorFilter!.setDefaults()
         colorFilter!.setValue(qrFilter!.outputImage, forKey: "inputImage")
         colorFilter!.setValue(color, forKey: "inputColor0")
         colorFilter!.setValue(backgroundColor, forKey: "inputColor1")
-        
         let transformedImage = createNonInterpolatedUIImageFromCIImage(colorFilter!.outputImage!, withScale: 8.0)
-
         image_holder.image = transformedImage
         
     }
     
     func createNonInterpolatedUIImageFromCIImage(image: CIImage, withScale scale: CGFloat) -> UIImage {
-        var cgImage: CGImageRef = CIContext(options: nil).createCGImage(image, fromRect: image.extent)
+        let cgImage: CGImageRef = CIContext(options: nil).createCGImage(image, fromRect: image.extent)
         UIGraphicsBeginImageContext(CGSizeMake(image.extent.size.width * scale, image.extent.size.width * scale))
-        var context: CGContextRef = UIGraphicsGetCurrentContext()!
+        let context: CGContextRef = UIGraphicsGetCurrentContext()!
         CGContextSetInterpolationQuality(context, CGInterpolationQuality.None)
         CGContextDrawImage(context, CGContextGetClipBoundingBox(context), cgImage)
-        var scaledImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        let scaledImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return scaledImage
     }
